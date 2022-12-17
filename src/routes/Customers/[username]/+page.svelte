5966d85864c505d99f9bae38824c5b5781618c1e
Hazel Anne Mejias <hazelannemejias1@gmail.com>
2022-12-17 11:41:04 +0800

    removed the current password

--- src/routes/Customers/[username]/+page.svelte
+++ src/routes/Customers/[username]/+page.svelte
@@ -182,7 +182,7 @@
             </div>
             <div class="column is-3 is-offset-2">
                 <p class="pText has-text-link ml-4 mb-1">
-                    <span>*</span>  Password: {customerDetails.customer_password}
+                    <span>*</span>  Password:
                 </p>
                 <input class="pText input is-rounded" type="password" bind:value={newCustomer.customerPassword}/>
             </div>
